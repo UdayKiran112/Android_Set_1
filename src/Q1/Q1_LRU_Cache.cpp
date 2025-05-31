b646d97308c02e5f6a49fb25dac2941dcc911e20
UdayKiran112 <udaykiranuk1126@gmail.com>
2025-05-31 11:46:49 +0530

    Refactor LRUCache implementation for improved readability and consistency

--- src/Q1/Q1_LRU_Cache.cpp
+++ src/Q1/Q1_LRU_Cache.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <unordered_map>
+#include <algorithm>
 #include <list>
-#include <sstream>
+#include <string>
 using namespace std;
 
-class LRUCache {
+class LRUCache
+{
 private:
     int capacity;
     list<int> usage;
     unordered_map<int, pair<int, list<int>::iterator>> cache;
 
 public:
-    LRUCache(int capacity) {
+    LRUCache(int capacity)
+    {
         this->capacity = capacity;
     }
 
-    int get(int key) {
+    int get(int key)
+    {
         if (cache.find(key) == cache.end())
             return -1;
         usage.erase(cache[key].second);
@@ -24,10 +28,14 @@ public:
         return cache[key].first;
     }
 
-    void put(int key, int value) {
-        if (cache.find(key) != cache.end()) {
+    void put(int key, int value)
+    {
+        if (cache.find(key) != cache.end())
+        {
             usage.erase(cache[key].second);
-        } else if (cache.size() == capacity) {
+        }
+        else if (cache.size() == capacity)
+        {
             int lru = usage.back();
             usage.pop_back();
             cache.erase(lru);
@@ -37,35 +45,44 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     string line;
-    LRUCache* lru = nullptr;
+    LRUCache *lru = nullptr;
 
-    while (getline(cin, line)) {
-        if (line.empty()) continue;
+    while (getline(cin, line))
+    {
+        // Remove spaces
+        line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
 
-        // Trim whitespace
-        while (!line.empty() && isspace(line.back())) line.pop_back();
+        if (line == "exit;" || line == "exit")
+            break;
 
-        if (line == "exit;" || line == "exit") break;
-
-        if (line.find("LRUCache") != string::npos) {
-            int capStart = line.find('(') + 1;
-            int capEnd = line.find(')');
-            int capacity = stoi(line.substr(capStart, capEnd - capStart));
+        if (line.find("LRUCache") != string::npos)
+        {
+            // Format: LRUCachelru(2);
+            int start = line.find('(') + 1;
+            int end = line.find(')');
+            int capacity = stoi(line.substr(start, end - start));
             lru = new LRUCache(capacity);
-        } else if (line.find("put") != string::npos) {
-            int argStart = line.find('(') + 1;
-            int argEnd = line.find(')');
-            string args = line.substr(argStart, argEnd - argStart);
+        }
+        else if (line.find("lru.put") != string::npos)
+        {
+            // Format: lru.put(1,1);
+            int start = line.find('(') + 1;
+            int end = line.find(')');
+            string args = line.substr(start, end - start);
             int comma = args.find(',');
             int key = stoi(args.substr(0, comma));
             int value = stoi(args.substr(comma + 1));
             lru->put(key, value);
-        } else if (line.find("get") != string::npos) {
-            int argStart = line.find('(') + 1;
-            int argEnd = line.find(')');
-            int key = stoi(line.substr(argStart, argEnd - argStart));
+        }
+        else if (line.find("lru.get") != string::npos)
+        {
+            // Format: lru.get(1);
+            int start = line.find('(') + 1;
+            int end = line.find(')');
+            int key = stoi(line.substr(start, end - start));
             cout << lru->get(key) << endl;
         }
     }
